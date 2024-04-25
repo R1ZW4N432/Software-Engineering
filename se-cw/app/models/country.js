68ab0187e1e7fcceb05f7b69f13abd4b0251c9b5
ahmedz777 <159578433+ahmedz777@users.noreply.github.com>
2024-04-25 11:57:09 +0100

    Update country.js

--- se-cw/app/models/country.js
+++ se-cw/app/models/country.js
@@ -1,1 +1,26 @@
+const db = require('../services/db');
 
+const Country = {
+    findAll: ({ continent, region, n }) => {
+        let whereClauses = [];
+        let sql = 'SELECT Code, Name, Continent, Region, Capital, Population FROM country';
+
+        if (continent && continent !== "All Continents") {
+            whereClauses.push(`Continent = '${continent}'`);
+        }
+        if (region && region !== "All Regions") {
+            whereClauses.push(`Region = '${region}'`);
+        }
+        if (whereClauses.length) {
+            sql += ' WHERE ' + whereClauses.join(' AND ');
+        }
+        sql += ' ORDER BY Population DESC';
+        if (n) {
+            sql += ` LIMIT ${parseInt(n)}`;
+        }
+
+        return db.query(sql);
+    }
+};
+
+module.exports = Country;
