bc793f9caae566feb15bfdada66352a857057213
Ridzyy15 <159798524+Ridzyy15@users.noreply.github.com>
2024-04-25 12:17:51 +0100

    Update countryController.js

--- se-cw/app/controllers/countryController.js
+++ se-cw/app/controllers/countryController.js
@@ -1,1 +1,17 @@
+// countryController.js
+const CountryModel = require('../models/country'); // Ensure this matches the name of your country model file
+
+const countryController = {
+    list: (req, res) => {
+        const { continent, region, n } = req.query;
+        CountryModel.findAll({ continent, region, n })
+            .then(countries => res.render('countries', { rows: countries, activePage: 'countries' }))
+            .catch(err => {
+                console.error('Error fetching countries:', err);
+                res.status(500).send('Internal Server Error');
+            });
+    }
+};
+
+module.exports = countryController;
 
